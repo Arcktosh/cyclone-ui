3823b7018035128e0fd1eeecbc04eea1b7240855
Ben Henning <benhenning10@gmail.com>
2022-06-13 14:07:22 +0200

    init

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export interface ButtonProps {
+label: string;
+}
+const Button = (props: ButtonProps) => {
+return <button>{props.label}</button>;
+};
+
+export default Button;
