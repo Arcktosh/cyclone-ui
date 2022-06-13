2b0596762fb4e5251efc84adedf5963d8e26221c
Ben Henning <benhenning10@gmail.com>
2022-06-14 01:18:29 +0200

    Daisy UI added

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,20 @@
 import "./Button.css"
 export interface ButtonProps {
 label?: string;
+bgColor?: "primary"|"secondary"|"success"|"danger"|"warning"|"info"|"light"|"dark";
+isOutlined?: boolean;
+width?: number;
+utilClass?: string;
+icon?: string;
+onClick?: () => void;
 }
 const Button = (props: ButtonProps) => {
-return <button className="text-3xl font-bold underline">{props.label}</button>;
+    const bgColor = props.bgColor ? `btn-${props.bgColor}` : "";
+    const width = props.width ? `w-${props.width}` : "";
+    const utilClass = props.utilClass ? `${props.utilClass}` : "";
+    const isOutlined = props.isOutlined ? 'btn-outline' : "";
+
+return <button className={`btn ${bgColor} ${isOutlined} ${width} ${utilClass}`} onClick={()=>props.onClick}>{props.icon} {props.label}</button>;
 };
 
 export default Button;
