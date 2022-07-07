9ef13b22039881d0deb5c93c1b754be39a6b7472
Ben Henning <benhenning10@gmail.com>
2022-07-07 21:09:37 +0200

    Added a QRCode Generator for text and VCards

---
+++ src/components/QRCode/QRCode.stories.tsx
@@ -0,0 +1,48 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+import Code from "./QRCode"
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Components/QRCode",
+  component: Code,
+} as ComponentMeta<typeof Code>
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Code> = (args) => <Code {...args} />
+
+export const Basic = Template.bind({})
+Basic.args = {
+  link: "https://github.com",
+}
+
+export const ColorQRCode = Template.bind({})
+ColorQRCode.args = {
+  link: "https://api.lorem.space/image/face?hash=92048",
+  bgColor: "#4D9AFF",
+  fgColor: "#FFFFFF",
+}
+export const QRCodeWithImage = Template.bind({})
+QRCodeWithImage.args = {
+  link: "https://api.lorem.space/image/face?hash=92048",
+  imageSettings: {
+    src: "https://api.lorem.space/image/face?hash=92048",
+    x: 51,
+    y: 51,
+    height: 24,
+    width: 24,
+    excavate: true,
+  },
+}
+export const QRCodeWithVCard = Template.bind({})
+QRCodeWithVCard.args = {
+  vCard: {
+    firstname: "John",
+    lastname: "Doe",
+    email: "Doe@email.com",
+    workPhone: "+1-555-555-5555",
+    url: "https://github.com",
+    company: "Cyclone UI",
+    street: "123 Main St",
+    country: "United States",
+  },
+}
---
+++ src/components/QRCode/QRCode.tsx
@@ -0,0 +1,117 @@
+import "./QRCode.css"
+import Code from "qrcode.react"
+import VCard from "vcard-creator"
+
+type ImageSettings = {
+  src?: string
+  x?: number
+  y?: number
+  height?: number
+  width?: number
+  excavate?: boolean
+}
+
+type VCardProps = {
+  firstname?: string
+  lastname?: string
+  additional?: string
+  prefix?: string
+  suffix?: string
+  company?: string
+  jobtitle?: string
+  role?: string
+  email?: string
+  workPhone?: string
+  extended?: string
+  street?: string
+  city?: string
+  region?: string
+  zip?: string
+  country?: string
+  addressType?: string
+  url?: string
+}
+
+export interface QRCodeProps {
+  link?: string
+  renderAs?: "svg" | "canvas"
+  size?: number
+  bgColor?: string
+  fgColor?: string
+  level?: "L" | "M" | "Q" | "H"
+  includeMargin?: boolean
+  imageSettings?: ImageSettings
+  vCard?: VCardProps
+}
+
+const QRCode = (props: QRCodeProps) => {
+  const myVCard = new VCard()
+
+  if (props.vCard) {
+    myVCard
+      .addName(
+        props.vCard?.lastname,
+        props.vCard?.firstname,
+        props.vCard?.additional,
+        props.vCard?.prefix,
+        props.vCard?.suffix
+      )
+      .addCompany(props.vCard.company ? props.vCard.company : "")
+      .addJobtitle(props.vCard.jobtitle ? props.vCard.jobtitle : "")
+      .addRole(props.vCard.role ? props.vCard.role : "")
+      .addEmail(props.vCard.email ? props.vCard.email : "")
+      .addPhoneNumber(
+        props.vCard.workPhone ? props.vCard.workPhone : "",
+        "PREF;WORK"
+      )
+      .addPhoneNumber(
+        props.vCard.workPhone ? props.vCard.workPhone : "",
+        "WORK"
+      )
+      .addAddress(
+        props.vCard?.company,
+        props.vCard?.extended,
+        props.vCard?.street,
+        props.vCard?.city,
+        props.vCard?.region,
+        props.vCard?.zip,
+        props.vCard?.country,
+        props.vCard?.addressType
+      )
+      .addURL(props.vCard.url ? props.vCard.url : "")
+  }
+
+  return props.imageSettings ? (
+    <Code
+      value={props.link ? props.link : myVCard ? myVCard.toString() : ""}
+      renderAs={props.renderAs ? props.renderAs : "canvas"}
+      size={props.size ? props.size : 128}
+      bgColor={props.bgColor ? props.bgColor : "#FFFFFF"}
+      fgColor={props.fgColor ? props.fgColor : "#000000"}
+      level={props.level ? props.level : "L"}
+      includeMargin={props.includeMargin ? props.includeMargin : false}
+      imageSettings={{
+        src: props.imageSettings.src ? props.imageSettings.src : "",
+        x: props.imageSettings.x,
+        y: props.imageSettings.y,
+        height: props.imageSettings.height ? props.imageSettings.height : 24,
+        width: props.imageSettings.width ? props.imageSettings.width : 24,
+        excavate: props.imageSettings.excavate
+          ? props.imageSettings.excavate
+          : true,
+      }}
+    />
+  ) : (
+    <Code
+      value={props.link ? props.link : myVCard ? myVCard.toString() : ""}
+      renderAs={props.renderAs ? props.renderAs : "canvas"}
+      size={props.size ? props.size : 128}
+      bgColor={props.bgColor ? props.bgColor : "#FFFFFF"}
+      fgColor={props.fgColor ? props.fgColor : "#000000"}
+      level={props.level ? props.level : "L"}
+      includeMargin={props.includeMargin ? props.includeMargin : false}
+    />
+  )
+}
+
+export default QRCode
